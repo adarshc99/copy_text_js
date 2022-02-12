2453fb3a22e8160e257e9f67cc879f1a5afe1bcb
adarshc99 <99551250+adarshc99@users.noreply.github.com>
2022-02-12 22:25:03 +0530

    Add files via upload

---
+++ html css js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+    
+});
+
+
+function copy()
+{
+    
+    $("#h2").html("Message Copied");
+    var a = $("#input-text").val();
+    console.log("hiii");
+    console.log(a);
+
+}
